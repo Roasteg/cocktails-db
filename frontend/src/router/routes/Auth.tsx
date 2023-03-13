352e912fa73e415e35896adc52e639e74347009d
Roasteg <krostislav468@gmail.com>
2023-03-13 14:44:15 +0200

    Login page in progress

--- frontend/src/router/routes/Auth.tsx
+++ frontend/src/router/routes/Auth.tsx
@@ -1,13 +1,26 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 
 const Auth = () => {
     return (
         <>
-            <div className="flex shadow-sm rounded-sm justify-center items-center mb-2">
-                <form className="mt-auto" action="" onSubmit={(e: FormEvent)=>{
+            <div className="flex h-screen">
+                <form className="m-auto lg:w-3/6 transition-all shadow-md rounded-lg justify-center items-center p-10" action="" onSubmit={(e: FormEvent) => {
                     e.preventDefault();
                 }}>
-                    <input placeholder="Text"/>
+                    <div className="flex flex-col transition-all ">
+                        <label htmlFor="login" className="mb-4 font-semibold">Login</label>
+                        <input id="login" placeholder="Login" className="form-input login"/>
+                        <label htmlFor="password" className="mt-5 mb-4 font-semibold">Password</label>
+                        <input id="password" placeholder="Password" type={"password"} className="form-input password" />
+                        <a className="w-full p-3 rounded-xl shadow-sm bg-black text-white text-center mt-6 font-semibold cursor-pointer">Sign in</a>
+                        <div className="flex justify-center space-x-1 mt-3">
+                            <p className="text-gray-600">Don't have an account?</p>
+                            <a href="#" onClick={(e)=>{
+                                e.preventDefault();
+                            }} className="text-blue-600 font-semibold transition-all hover:text-blue-700">Sign up</a>
+                        </div>
+                    </div>
+
                 </form>
             </div>
         </>
